a7476229f0a1b2c7ce679d02afd5abe57aa77b1f
Mikhail Vazhenin <mikervzhik@gmail.com>
2016-04-06 23:44:19 +0300

    added build.bat and first version of package

--- build/build.bat
+++ build/build.bat
@@ -1,1 +1,4 @@
-.\..\tools\nuget\nuget.exe pack Elmah.Gelf.nuspec -version 1.0.0
+..\tools\nuget\nuget.exe pack Elmah.Gelf.nuspec -version %1
+..\tools\nuget\nuget.exe push Elmah.Gelf.%1.nupkg
+   
+  
